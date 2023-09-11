4e2a73dd7d12ca558e63579cdf291e13c6a8cc39
JaydenLee1116 <vv55adss@gmail.com>
2023-09-11 17:40:22 +0900

    🐛 fix: useGetData 훅 반환값 수정
    - data에서 data.data로 수정

--- src/services/useGetData/index.ts
+++ src/services/useGetData/index.ts
@@ -6,6 +6,6 @@ import { MOCK_API_BASE_URL } from '../../api/apiConfig';
 export const useGetData = () => {
   return useQuery(['data'], async () => {
     const { data } = await axiosFetch.get(MOCK_API_BASE_URL);
-    return data;
+    return data.data;
   });
 };
