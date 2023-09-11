a2c0ef2f031ece4e0224852373c3eafe7a2e18ea
JaydenLee1116 <vv55adss@gmail.com>
2023-09-12 00:22:18 +0900

    ✨ feat: useGetData에 데이터를 변환하는 함수 추가 구현
    - 현재 프로젝트에서의 useGetData의 역할을 고려하여 추가

--- src/services/useGetData/index.ts
+++ src/services/useGetData/index.ts
@@ -2,10 +2,19 @@ import { useQuery } from '@tanstack/react-query';
 
 import { axiosFetch } from '../../api/apiClient';
 import { MOCK_API_BASE_URL } from '../../api/apiConfig';
+import { getArrayWithKeyFromObject } from '../../utils/getArrayWithKeyFromObject';
+import { getArrayWithSplittedValue } from '../../utils/getArrayWithSplittedValue';
+import { NEW_FIELD_NAME_FROM_OBJECT } from '../../constants';
 
 export const useGetData = () => {
   return useQuery(['data'], async () => {
     const { data } = await axiosFetch.get(MOCK_API_BASE_URL);
-    return data.data;
+    const { response } = data.data;
+    return getArrayWithSplittedValue(
+      getArrayWithKeyFromObject(response, NEW_FIELD_NAME_FROM_OBJECT),
+      NEW_FIELD_NAME_FROM_OBJECT,
+      ' ',
+      1,
+    );
   });
 };
