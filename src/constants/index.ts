898a3bb7d7a162219f3f1ca1d39a76e65cf38020
JaydenLee1116 <vv55adss@gmail.com>
2023-09-12 22:49:25 +0900

    ✨ feat: 주요 상수 업데이트

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,6 @@
 export const TIME_TO_NAVIGATE_HOME_PAGE = 3000;
 export const NEW_FIELD_NAME_FROM_OBJECT = 'time';
+export const TOTAL_ID = '전체';
+export const COPYRIGHT = 'ⓒ 2023. Wanted JaydenLee1116 ALL RIGHTS RESERVED.';
+export const Y_AXIS_ID_LEFT = 'y-axis-left';
+export const Y_AXIS_ID_RIGHT = 'y-axis-right';
