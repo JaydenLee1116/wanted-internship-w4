d5c4df268ed1bc6918b5ec3261a3efb7c1e8600f
JaydenLee1116 <vv55adss@gmail.com>
2023-09-13 18:30:04 +0900

    ✨ feat: Chart title 추가

---
+++ src/components/Chart/Chart.styled.ts
@@ -0,0 +1,8 @@
+import { styled } from 'styled-components';
+
+export const Title = styled.h1`
+  color: ${({ theme }) => theme.colors.blue};
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 16px;
+`;
--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -21,6 +21,7 @@ import {
   Y_AXIS_ID_LEFT,
   Y_AXIS_ID_RIGHT,
 } from '../../constants';
+import * as S from './Chart.styled';
 
 interface ChartProps {
   data?: Datum[];
@@ -31,53 +32,56 @@ interface ChartProps {
 export const Chart = ({ data, selectedGuId, onClick }: ChartProps) => {
   const theme = useTheme();
   return (
-    <ResponsiveContainer width="80%" height="80%">
-      <ComposedChart width={1200} height={640} data={data}>
-        <CartesianGrid stroke={theme.colors.darkGray} strokeDasharray={GRID_STROKE_DASH_ARRAY} />
-        <XAxis dataKey="time" stroke={theme.colors.black} interval={X_AXIS_INTERVAL} />
-        <YAxis
-          yAxisId={Y_AXIS_ID_RIGHT}
-          dataKey="value_bar"
-          orientation="right"
-          stroke={theme.colors.sub.primary}
-        />
-        <Bar
-          yAxisId={Y_AXIS_ID_RIGHT}
-          type="monotone"
-          dataKey="value_bar"
-          stroke={theme.colors.sub.primary}
-          fill={theme.colors.sub.secondary}
-          shape={(props: RectangleProps) => {
-            const { id } = props;
-            return (
-              <Rectangle
-                {...props}
-                fill={id === selectedGuId ? theme.colors.red : theme.colors.sub.secondary}
-                onClick={() => {
-                  if (!id) return;
-                  onClick(id);
-                }}
-              />
-            );
-          }}
-        />
-        <YAxis
-          yAxisId={Y_AXIS_ID_LEFT}
-          dataKey="value_area"
-          orientation="left"
-          domain={[0, 200]}
-          stroke={theme.colors.main.primary}
-        />
-        <Area
-          yAxisId={Y_AXIS_ID_LEFT}
-          type="monotone"
-          dataKey="value_area"
-          stroke={theme.colors.main.primary}
-          fill={theme.colors.main.secondary}
-        />
-        <Tooltip />
-        <Legend />
-      </ComposedChart>
-    </ResponsiveContainer>
+    <>
+      <S.Title>{selectedGuId}</S.Title>
+      <ResponsiveContainer width="80%" height="80%">
+        <ComposedChart width={1200} height={640} data={data} title={selectedGuId}>
+          <CartesianGrid stroke={theme.colors.darkGray} strokeDasharray={GRID_STROKE_DASH_ARRAY} />
+          <XAxis dataKey="time" stroke={theme.colors.black} interval={X_AXIS_INTERVAL} />
+          <YAxis
+            yAxisId={Y_AXIS_ID_RIGHT}
+            dataKey="value_bar"
+            orientation="right"
+            stroke={theme.colors.sub.primary}
+          />
+          <Bar
+            yAxisId={Y_AXIS_ID_RIGHT}
+            type="monotone"
+            dataKey="value_bar"
+            stroke={theme.colors.sub.primary}
+            fill={theme.colors.sub.secondary}
+            shape={(props: RectangleProps) => {
+              const { id } = props;
+              return (
+                <Rectangle
+                  {...props}
+                  fill={id === selectedGuId ? theme.colors.red : theme.colors.sub.secondary}
+                  onClick={() => {
+                    if (!id) return;
+                    onClick(id);
+                  }}
+                />
+              );
+            }}
+          />
+          <YAxis
+            yAxisId={Y_AXIS_ID_LEFT}
+            dataKey="value_area"
+            orientation="left"
+            domain={[0, 200]}
+            stroke={theme.colors.main.primary}
+          />
+          <Area
+            yAxisId={Y_AXIS_ID_LEFT}
+            type="monotone"
+            dataKey="value_area"
+            stroke={theme.colors.main.primary}
+            fill={theme.colors.main.secondary}
+          />
+          <Tooltip />
+          <Legend />
+        </ComposedChart>
+      </ResponsiveContainer>
+    </>
   );
 };
