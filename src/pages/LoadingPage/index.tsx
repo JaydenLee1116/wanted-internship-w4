633095fe368b286ea3a1d6c80f4c2e53a8cc18fb
JaydenLee1116 <vv55adss@gmail.com>
2023-09-13 22:27:24 +0900

    ✨ feat: 로딩 페이지 추가

---
+++ src/pages/LoadingPage/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as S from './LoadingPage.styled';
+
+export const LoadingPage = () => {
+  return (
+    <S.Container>
+      <S.Content>로딩 중...</S.Content>
+    </S.Container>
+  );
+};
