3609debcbccf67861d5efc13f5ffcea2f1191e27
JaydenLee1116 <vv55adss@gmail.com>
2023-09-13 20:57:06 +0900

    ✨ feat: 배포 서버 api 추가

--- src/services/useGetData/index.ts
+++ src/services/useGetData/index.ts
@@ -1,15 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { axiosFetch } from '../../api/apiClient';
-import { MOCK_API_BASE_URL } from '../../api/apiConfig';
 import { getArrayWithKeyFromObject } from '../../utils/getArrayWithKeyFromObject';
 import { getArrayWithSplittedValue } from '../../utils/getArrayWithSplittedValue';
 import { NEW_FIELD_NAME_FROM_OBJECT } from '../../constants';
 
 export const useGetData = () => {
   return useQuery(['data'], async () => {
-    const { data } = await axiosFetch.get(MOCK_API_BASE_URL);
-    const { response } = data.data;
+    const { data } = await axiosFetch.get('/');
+    const { response } = data;
     return getArrayWithSplittedValue(
       getArrayWithKeyFromObject(response, NEW_FIELD_NAME_FROM_OBJECT),
       NEW_FIELD_NAME_FROM_OBJECT,
