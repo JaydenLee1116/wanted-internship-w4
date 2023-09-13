5b0f575a70fb59d7fabf9ad75caa1ef971b1929d
JaydenLee1116 <vv55adss@gmail.com>
2023-09-13 19:18:05 +0900

    🐛 fix: setup 함수명 겹치는 에러 해결

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,33 +1,32 @@
 describe('Cypress', () => {
   it('should render footer content "ⓒ 2023. Wanted JaydenLee1116 ALL RIGHTS RESERVED."', () => {
-    setup();
+    setupCypress();
     cy.get('footer')
       .contains('ⓒ 2023. Wanted JaydenLee1116 ALL RIGHTS RESERVED.')
       .should('be.visible');
   });
 
   it('should render title "성북구" when click "성북구" button', () => {
-    setup();
+    setupCypress();
     cy.get('button').contains('성북구').click();
     cy.get('h1').contains('성북구').should('be.visible');
   });
 
   it('should render red color filled "강남구" rectangles when click "강남구" button', () => {
-    setup();
+    setupCypress();
     const RED_COLOR = '#fa5252';
     cy.get('button').contains('강남구').click();
     cy.get('.recharts-rectangle').get('#강남구').should('have.attr', 'fill', RED_COLOR);
   });
 
   it('should render red color filled "노원구" rectangles when click any "노원구" rectangle', () => {
-    setup();
+    setupCypress();
     const RED_COLOR = '#fa5252';
-    console.log(cy.get('.recharts-rectangle').get('#노원구').first());
     cy.get('.recharts-rectangle').get('#노원구').first().click({ force: true });
     cy.get('.recharts-rectangle').get('#노원구').should('have.attr', 'fill', RED_COLOR);
   });
 });
 
-const setup = () => {
+const setupCypress = () => {
   cy.visit('http://localhost:3000');
 };
